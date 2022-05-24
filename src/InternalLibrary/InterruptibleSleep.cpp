0f33cf1e4a3b625a38330a9ca8f720f8e5ee72ac
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-05-23 20:26:02 -0400

    Fix failure to compile on linux

--- src/InternalLibrary/InterruptibleSleep.cpp
+++ src/InternalLibrary/InterruptibleSleep.cpp
@@ -16,12 +16,14 @@ void signal_handler(int signal) {
     caugth_signal = signal;
 }
 
+typedef void (*_old_signal_hanlder_type)(int);
+
 int sleep_for_x_milliseconds(int milliseconds_to_sleep_for) {
     caugth_signal = -1;
     std::mutex m;
     std::unique_lock<std::mutex> lock(m);
     std::vector<int> signals_to_handle = {SIGINT, SIGTERM};
-    std::unordered_map<int, _crt_signal_t> old_signal_handlers = {};
+    std::unordered_map<int, _old_signal_hanlder_type> old_signal_handlers = {};
 
     for (int signal : signals_to_handle) {
         old_signal_handlers[signal] = std::signal(signal, signal_handler);
@@ -32,8 +34,8 @@ int sleep_for_x_milliseconds(int milliseconds_to_sleep_for) {
 
     cv.wait_for(lock, std::chrono::milliseconds(milliseconds_to_sleep_for));
 
-    for (std::pair<int, _crt_signal_t> element : old_signal_handlers) {
-        auto response = std::signal(element.first, element.second);
+    for (std::pair<int, _old_signal_hanlder_type> element : old_signal_handlers) {
+        _old_signal_hanlder_type response = std::signal(element.first, element.second);
         if (response == SIG_ERR) {
             throw std::runtime_error(
                 "Failed to reset to initial signal handler");
